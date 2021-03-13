6575f7eb97490d02580f606369fc5221d66296f8
Felipe Leal Trovo <felipeltrovo@hotmail.com>
2021-03-13 20:43:46 -0300

    Adds a new partial for Home#Index. Small layout tweaks.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,17 @@
 class HomeController < ApplicationController
     def index
-        @study_items = StudyItem.all
+        load_study_items
+        load_study_items_statuses
     end
+
+    private
+
+        def load_study_items_statuses
+            @study_items_statuses = StudyItem.statuses
+        end
+
+        def load_study_items
+            @study_items = StudyItem.all
+        end
+
 end
