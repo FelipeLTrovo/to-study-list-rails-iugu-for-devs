81b70f6f6b4211f79e8ee63b54484082de338d1d
Felipe Leal Trovo <felipeltrovo@hotmail.com>
2021-03-13 15:30:10 -0300

    Adds Enum Status for StudyItem entity and tweaks _form.

--- app/models/study_item.rb
+++ app/models/study_item.rb
@@ -1,6 +1,12 @@
 class StudyItem < ApplicationRecord
   belongs_to :category
 
+  enum status: {
+    pending: 0,
+    in_progress: 1,
+    deleted: 2
+  }
+
   def full_title
     "#{title} - #{category.title}"
   end
